ddd0f46e07b0ddb1543162f3482a089846902c98
techiphyte76 <77113884+techiphyte76@users.noreply.github.com>
2021-03-28 09:31:18 -0400

    Added the fetching of assignment name and category. Created the method to create an array of objects that contain the info for each assignment. Extended callback function

--- fetchGrades.js
+++ fetchGrades.js
@@ -3,4 +3,26 @@ scores = [];
 for (i = 0; i < grades.length; i++) {
     scores[i] = grades[i].innerHTML; 
 }
-scores; 
+
+assignmentName = document.querySelectorAll('[data-col="TITLE"]');
+names = [];
+for (i = 0; i < assignmentName.length; i++) {
+    names[i] = assignmentName[i].innerHTML; 
+}
+
+assignmentType = document.querySelectorAll('[data-col="CATEGORY_ID"]');
+category = [];
+for (i = 0; i < assignmentType.length; i++) {
+    category[i] = assignmentType[i].innerHTML; 
+}
+
+let arr = [];
+for (i = 0 ; i < names.length; i++) {
+    arr[i] = {
+        name: names[i],
+        grades: scores[i],
+        type: category[i]
+    }
+}
+
+arr;
--- popup.js
+++ popup.js
@@ -1,30 +1,58 @@
-document.addEventListener('DOMContentLoaded', function(){
-    var checkPageButton = document.getElementById('average');
-    checkPageButton.addEventListener('click',function(){
-        chrome.tabs.getSelected(null, function(tab){
-            alert(calculateAverage());
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var checkPageButton = document.getElementById("average");
+    checkPageButton.addEventListener(
+      "click",
+      function () {
+        chrome.tabs.getSelected(null, function (tab) {
+          alert(calculateAverage());
         });
-    }, false);
-}, false);
+      },
+      false
+    );
+  },
+  false
+);
 
 function calculateAverage() {
-    var grade = "Hi";
-    var row = 1;
-    //setTimeout(() => grade = document.querySelector("#LOy_row1 > td:nth-child(2)").innerHTML, 1000);
-    grade =  String(document.getElementsByClassName("lo_export_csv")[0].getAttribute("href"));
-    return grade;
+  var grade = "Hi";
+  var row = 1;
+  //setTimeout(() => grade = document.querySelector("#LOy_row1 > td:nth-child(2)").innerHTML, 1000);
+  grade = String(
+    document.getElementsByClassName("lo_export_csv")[0].getAttribute("href")
+  );
+  return grade;
 }
 
 allGrades = [];
 
-function fetchGrade() {
-    chrome.tabs.executeScript(
-      {
-        file: "/fetchGrades.js"
-      },
-      results => {document.getElementById("totalGrades").innerHTML = results}
+function createGradeEntries(itemsArray) {
+  let test = [];
+  for (i = 0; i < itemsArray.length; i++) {
+    test[i] = document.createTextNode(
+      `Assignment: ${itemsArray[0].name}: ${itemsArray[0].grades} : ${itemsArray[0].type}`
     );
   }
-  
+  return test; 
+}
 
-  document.getElementById("clickme").addEventListener("click", fetchGrade);
+function fetchGrade() {
+  chrome.tabs.executeScript(
+    {
+      file: "/fetchGrades.js",
+    },
+    (results) => {
+      let objArray = results[0];
+      const newDiv = document.createElement("div");
+      const newContent = createGradeEntries(objArray);
+      for (i = 0; i < objArray.length; i++) {
+        newDiv.appendChild(newContent[i]);
+      } 
+      document.body.insertAdjacentElement("afterend", newDiv);
+      console.log(results[0][0]);
+    }
+  );
+}
+
+document.getElementById("clickme").addEventListener("click", fetchGrade);
